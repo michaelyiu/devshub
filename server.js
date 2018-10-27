4da1ce8b642f698b49a787f9334e72a29ced6691
Michael Yiu <onew1ng3d@gmail.com>
2018-10-27 14:35:14 -0400

    User model created, user registration route created

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -18,6 +24,8 @@ mongoose
 app.get("/", (req, res) => res.send("Hello"));
 
 //Use routes
+// console.log('test');
+
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
