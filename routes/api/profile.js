be842fadd76deb693ea6835451b424b44d3e8ef0
Michael Yiu <onew1ng3d@gmail.com>
2018-10-29 01:12:51 -0400

    POST education and experience routes added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -5,6 +5,8 @@ const passport = require('passport');
 
 //Load Validation
 const validateProfileInput = require('./../../validation/profile');
+const validateExperienceInput = require('./../../validation/experience');
+const validateEducationInput = require('./../../validation/education');
 
 //Load Profile and User Model
 const Profile = require('./../../models/Profile');
@@ -156,5 +158,71 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
 
 });
 
+
+// @route   GET api/profile/experience
+// @desc    Add experience to profile
+// @access  Private route
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const { errors, isValid } = validateExperienceInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    //returns any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+      }
+
+      //Add to exp array
+      profile.experience.unshift(newExp);
+
+      profile.save().then(profile => res.json(profile))
+    })
+})
+
+// @route   GET api/profile/education
+// @desc    Add education to profile
+// @access  Private route
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+  const { errors, isValid } = validateEducationInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    //returns any errors with 400 status
+    return res.status(400).json(errors);
+  }
+
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldOfStudy: req.body.fieldOfStudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description,
+      }
+
+      //Add to exp array
+      profile.education.unshift(newEdu);
+
+      profile.save().then(profile => res.json(profile))
+    })
+})
 
 module.exports = router;
