9c52b0bdeebb91b4cd0fdde4634c0222b480cca8
Michael Yiu <onew1ng3d@gmail.com>
2018-11-08 19:40:54 -0500

    delete comment added

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -24,11 +24,16 @@ class Navbar extends Component {
           <Link className="nav-link" to="/dashboard">Dashboard</Link>
         </li>
         <li className="nav-item">
+          <img className="rounded-circle m-2" src={user.avatar} alt={user.name} style={{ width: '25px', marginRight: '5px' }} title="You must have a Gravatar connected to your email to display an image" />
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/dashboard">{user.name}</Link>
+        </li>
+        <li className="nav-item">
           <a
             href="/#"
             onClick={this.onLogoutClick.bind(this)}
             className="nav-link">
-            <img className="rounded-circle" src={user.avatar} alt={user.name} style={{ width: '25px', marginRight: '5px' }} title="You must have a Gravatar connected to your email to display an image" />
             Logout
           </a>
         </li>
