df46739920381936f69f83c928f98b757234d79a
Michael Yiu <onew1ng3d@gmail.com>
2018-11-10 11:38:09 -0500

    bugfix in redux store creation

--- client/src/store.js
+++ client/src/store.js
@@ -8,13 +8,22 @@ const middleware = [thunk];
 
 //creates redux
 const store = createStore(
+  // rootReducer,
+  // initialState,
+  // compose(
+  //   applyMiddleware(...middleware),
+  //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // )
+
+
+
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-
+    window.navigator.userAgent.includes('Chrome') ?
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : compose,
+  ),
 );
 
 export default store;
